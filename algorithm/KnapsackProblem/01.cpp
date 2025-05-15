0108c768017fd3d5ea4ed5f6d1e919527222f7d0
northhalf <480563986@qq.com>
2025-05-15 18:37:31 +0800

    01背包

--- algorithm/KnapsackProblem/01.cpp
+++ algorithm/KnapsackProblem/01.cpp
@@ -1,20 +1,16 @@
 #include <algorithm>
 #include <iostream>
-int dp[10001][10001];
+int dp[100001];
 int main() {
-    int t, m;
-    std::cin >> t >> m;
-    for (int i = 1; i <= m; i++) {
+    int t, n;
+    std::cin >> t >> n;
+    for (int i = 0; i < n; i++) {
         int a, b;
         std::cin >> a >> b;
-        // 枚举背包容量
-        for (int j = 0; j <= t; j++) {
-            if (j >= a)
-                dp[i][j] = std::max(dp[i - 1][j - a] + b, dp[i - 1][j]);
-            else  // 如果容量不够装下这个物体，则放置之前获得的最大价值
-                dp[i][j] = dp[i - 1][j];
+        for (int j = t; j >= a; j--) {
+            dp[j] = std::max(dp[j], dp[j - a] + b);
         }
     }
-    std::cout << dp[m][t] << std::endl;
+    std::cout << dp[t] << std::endl;
     return 0;
 }
