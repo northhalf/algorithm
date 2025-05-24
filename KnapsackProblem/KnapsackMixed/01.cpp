69e7459228ce540234a4b1745ed28ff37003c60c
northhalf <480563986@qq.com>
2025-05-24 13:35:56 +0800

    多重背包

--- KnapsackProblem/KnapsackMixed/01.cpp
+++ KnapsackProblem/KnapsackMixed/01.cpp
@@ -17,6 +17,65 @@ void QuickWrite(T num);
 
 int main() {
     std::function qr = QuickRead<ll>;
+    // 货币种类和预期价值
+    ll sorts_currency = qr(), aid_value = qr();
+    // 存储货币金额种类
+    std::vector<ll> currency(sorts_currency + 1);
+    // 存储二进制拆分结果
+    std::vector<std::pair<ll, ll>> binary_currency;
+    binary_currency.reserve(sorts_currency * 100);
+    // 记录最大金额
+    ll max_currency = 0;
+
+    // 记录货币金额
+    for (ll i = 1; i <= sorts_currency; i++) {
+        currency[i] = qr();
+        max_currency = std::max(max_currency, currency[i]);
+    }
+    // 记录各货币的张数，并进行二进制拆分
+    for (ll i = 1; i <= sorts_currency; i++) {
+        ll num = qr();
+        for (ll j = 1; j <= num; num -= j, j <<= 1) {
+            binary_currency.emplace_back(j, j * currency[i]);
+        }
+        if (num != 0) {
+            binary_currency.emplace_back(num, num * currency[i]);
+        }
+    }
+
+    constexpr ll max_ll = 0x3f3f3f3f3f3f3f3f;  // 设定一个long long较大值
+    // 最大上届为T+max_V^2
+    ll upper_bound = aid_value + max_currency * max_currency;
+    // 两个dp数组分别记录John和店主付出不同金额的最小张数
+    std::vector<ll> dp1(upper_bound + 1, max_ll), dp2(upper_bound + 1, max_ll);
+    dp1[0] = dp2[0] = 0;
+
+    // 对Johb使用多次01背包
+    // 枚举物品
+    for (size_t i = 0; i < binary_currency.size(); i++) {
+        // 枚举使出钱数
+        for (ll j = upper_bound; j >= binary_currency[i].second; j--) {
+            dp1[j] = std::min(
+                dp1[j - binary_currency[i].second] + binary_currency[i].first,
+                dp1[j]
+            );
+        }
+    }
+    // 对店主使用完全背包
+    // 枚举物品
+    for (ll i = 0; i < sorts_currency; i++) {
+        for (ll j = currency[i]; j <= upper_bound; j++) {
+            dp2[j] = std::min(dp2[j - currency[i]] + 1, dp2[j]);
+        }
+    }
+
+    // 查找最小组合
+    ll ans = max_ll;
+    for (ll i = aid_value; i <= upper_bound; i++) {
+        ans = std::min(ans, dp1[i] + dp2[i - aid_value]);
+    }
+    // 根据ans值是否有变化判断是否存在解
+    (ans == max_ll) ? (QuickWrite(-1)) : (QuickWrite(ans));
     return 0;
 }
 
